30ca3f9260f23d6d5d1929329c681d099d10eeda
sadoMasupilami <michiklug85@gmail.com>
2023-09-18 13:54:12 +0000

    init

---
+++ main.tf
@@ -0,0 +1,15 @@
+variable "infoblox_user" {
+  
+}
+
+variable "infoblox_password" {
+  
+}
+
+output "infoblox_user" {
+  value = var.infoblox_user
+}
+
+output "infoblox_password" {
+  value = var.infoblox_password
+}
